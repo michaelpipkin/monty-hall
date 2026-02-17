ce7510a5a273ea4cb27bf10d29267e74aabc7244
michaelpipkin <michael.a.pipkin@gmail.com>
2026-02-17 12:37:11 -0500

    Remove deprecated provider from app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,7 @@ import {
   ApplicationConfig,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection(), provideAnimationsAsync()],
+  providers: [provideZonelessChangeDetection()],
 };
